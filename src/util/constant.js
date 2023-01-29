44fa90372fcda00919480112738054c5ec91bb73
Md Bakibillah Rohan <mbakibillahrohan@gmail.com>
2023-01-29 14:40:33 +0600

    initially some list are completed

--- src/util/constant.js
+++ src/util/constant.js
@@ -11,7 +11,12 @@ module.exports = {
         LOGIN: "login",
         LOGIN_LOG: "login_log",
         ROLE: "role",
-        CUSTOMER: "customer"
+        CUSTOMER: "customer",
+        TICKET: "ticket",
+        VENDOR: "supplier",
+        ACCOUNT: "account",
+        EMPLOYEE: "employee",
+        DEPARTMENT: "department",
     },
 
     API: {
@@ -20,9 +25,24 @@ module.exports = {
         SIGN_IN: "/v1/sign-in",
         SIGN_OUT: "/v1/sign-out",
 
-        // Customer Endpoint 
+        // Customer Endpoints
         GET_CUSTOMER_LIST: "/v1/customer/get-list",
         SAVE_CUSTOMER: "/v1/customer/save",
-        UPDATE_CUSTOMER: "/v1/customer/update"
+        UPDATE_CUSTOMER: "/v1/customer/update",
+
+        // Ticket Endpoints 
+        GET_TICKET_LIST: "/v1/ticket/get-list",
+
+        // Vendor Endpoints 
+        GET_VENDOR_LIST: "/v1/vendor/get-list",
+
+        // Account Enpoints 
+        GET_ACCOUNT_LIST: "/v1/account/get-list",
+
+        // Employee Endpoints
+        GET_EMPLOYEE_LIST: "/v1/employee/get-list",
+
+        // Department Endpoints 
+        GET_DEPARTMENT_LIST: "/v1/department/get-list",
     }
 };
