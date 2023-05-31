0c2a24903dda4e6300c0edb04f834e3ef4247574
Md Bakibillah Rohan <mbakibillahrohan@gmail.com>
2023-05-31 17:31:30 +0600

    payment receive save

--- src/routes/payment/payment received/get_due_invoice_list.js
+++ src/routes/payment/payment received/get_due_invoice_list.js
@@ -3,76 +3,65 @@
 const Joi = require("@hapi/joi");
 const log = require("../../../util/log");
 const Dao = require("../../../util/dao");
-const { API, MESSAGE, TABLE, CONSTANT } = require("../../../util/constant");
+const { API, MESSAGE, TABLE } = require("../../../util/constant");
 const { autheticatedUserInfo } = require("../../../util/helper");
 
 const query_scheme = Joi.object({
-    search_text: Joi.string().trim().min(1).optional(),
-    customer_oid: Joi.string().trim().min(1).optional(),
-    supplier_oid: Joi.string().trim().min(1).optional(),
-    reference_oid: Joi.string().trim().min(1).optional(),
-    payment_type: Joi.string().trim().min(1).optional(),
-    reference_type: Joi.string().trim().min().optional(),
-    payment_nature: Joi.string().trim().min(1).optional()
-})
+    customer_oid: Joi.string().trim().min(1).required(),
+    status: Joi.string().trim().valid("Active", "Inactive").required(),
 
-const route_controller = {
+    search_text: Joi.string().trim().min(1).optional(),
+});
+
+const get_list = {
     method: "GET",
-    path: API.CONTEXT + API.PAYMENT_RECEIVED_GET_DUE_INVOICE_LIST,
+    path: API.CONTEXT + API.PAYMENT_RECEIVED_GET_DUE_INVOICE_LIST_PATH,
     options: {
         auth: {
             mode: "required",
             strategy: "jwt",
         },
-        description: "Get due invoice list",
-        plugins: {
-            hapiAuthorization: false,
-        },
+        description: "Payment receive get due invoice list",
+        plugins: { hapiAuthorization: false },
         validate: {
             query: query_scheme,
             options: {
                 allowUnknown: false,
             },
             failAction: async (request, h, err) => {
-                return h.response({
-                    code: 400, status: false, message: err
-                }).takeover();
+                return h.response({ code: 400, status: false, message: err }).takeover();
             },
         },
     },
     handler: async (request, h) => {
         log.info(`Request received - ${JSON.stringify(request.query)}`);
         const response = await handle_request(request);
-        log.debug(`Response sent - ${JSON.stringify(response)}`)
+        log.debug(`Response sent - ${JSON.stringify(response)}`);
         return h.response(response);
     },
 };
 
 const handle_request = async (request) => {
     try {
+        const userInfo = await autheticatedUserInfo(request);
+        request.query.userInfo = userInfo;
         let count = await get_count(request);
-
         let data = [];
         if (count == 0) {
-            log.info(MESSAGE.NO_DATA_FOUND);
-            return {
-                status: false,
-                code: 400,
-                message: MESSAGE.NO_DATA_FOUND
-            };
+            log.info(`No data found`);
+            return { status: false, code: 400, message: `No data found` };
         }
-        data = await get_data(request);
-        log.info(`[${count}] found`)
-
+        data = await get_data(request)
+        log.info(`[${count}] found`);
         return {
             status: true,
             code: 200,
             message: MESSAGE.SUCCESS_GET_LIST,
-            count,
-            data,
+            data: data,
+            count: count
         };
     } catch (err) {
-        log.error(`An exception occurred while getting supplier list data: ${err}`);
+        log.error(`An exception occurred while getting payment due invoice list data : ${err}`);
         return {
             status: false,
             code: 500,
@@ -80,95 +69,64 @@ const handle_request = async (request) => {
         };
     }
 };
+
 const get_count = async (request) => {
-    let userInfo = await autheticatedUserInfo(request)
+    const { customer_oid, status, userInfo } = request.query;
     let count = 0;
-    let data = [];
-
-    let query = `select count(oid) from  ${TABLE.PAYMENT} where 1 = 1 and companyOid = $1 and referenceType = $2  and (paymentNature = $3 OR paymentNature = $4) `;
-
-    data.push(userInfo.companyoid, CONSTANT.CUSTOMER, CONSTANT.CREDIT_NOTE_ADJUSTMENT, CONSTANT.CASH_ADJUSTMENT);
-
-    let idx = 5;
-
-    if (request.query["referenceOid"]) {
-        query += ` and referenceOid = $${idx++}`
-        data.push(request.query["referenceOid"])
+    let data = [customer_oid, userInfo.companyoid, status];
+    let query = `select count(t.oid) as total
+    from ${TABLE.TICKET_INVOICE} t 
+    where t.customerOid = $1 and t.companyOid = $2 and t.status = $3 
+    and (t.receivableAmount - (ticket_invoice_received_amount(t.customeroid, t.oid))) > 0`;
+    if (request.query['search_text']) {
+        const searchText = '%' + request.query['search_text'].trim().toLowerCase() + '%';
+        query += ` and t.invoiceNo ilike $4`;
+        data.push(searchText);
     }
-    if (request.query["referenceType"]) {
-        query += ` and referenceType = $${idx++}`
-        data.push(request.query["referenceType"])
-    }
-    if (request.query["paymentType"]) {
-        query += ` and paymentType = $${idx++}`
-        data.push(request.query["paymentType"])
-    }
-    if (request.query["paymentNature"]) {
-        query += ` and paymentNature = $${idx++}`
-        data.push(request.query["paymentNature"])
-    }
-
     let sql = {
         text: query,
         values: data
     }
     try {
         let data_set = await Dao.get_data(request.pg, sql);
-        count = data_set[0]["count"];
-        log.info(count)
-    } catch (err) {
-        log.error(err)
-        throw new Error(err);
+        count = data_set[0]["total"];
+    } catch (e) {
+        throw new Error(e);
     }
     return count;
-}
+};
+
 const get_data = async (request) => {
-    const userInfo = await autheticatedUserInfo(request);
+    const { customer_oid, status, userInfo } = request.query;
     let list_data = [];
-    let data = [];
-    let query = `select p.oid, p.paymentNo as "payment_no", to_char(p.paymentDate, 'YYYY-MM-DD') as "payment_date", to_char(p.paymentDate :: DATE, 'dd-Mon-yyyy') as "payment_date_en", p.entryType as "entry_type", p.checkNo as "check_no", p.status, p.paymentType as "payment_type", p.referenceOid as "reference_oid", p.referenceType as "reference_type", p.amount, a.name as "account_name", p.accountOid as "account_oid", p.description, p.imagePath as "image_path", p.paymentNature as "payment_nature", (CASE WHEN p.referenceType = 'Customer' THEN c.name ELSE s.name END) as "reference_name" from  ${TABLE.PAYMENT} as p left join ${TABLE.CUSTOMER} as c on p.referenceOid = c.oid left join  ${TABLE.SUPPLIER} as s on p.referenceOid = s.oid left join ${TABLE.ACCOUNT} as a on p.accountOid = a.oid where 1 = 1 and p.companyOid = $1 and p.referenceType = $2  and (p.paymentNature = $3 OR p.paymentNature = $4) `;
+    let data = [customer_oid, userInfo.companyoid, status];
+    let query = `select 'Ticket' as "invoice_type", t.source, t.supplierOid as "supplier_oid", t.customerOid as "customer_oid", t.companyOid as "company_oid", t.oid, t.status, 
+    t.invoiceNo as "invoice_no", to_char(t.invoiceDate, 'YYYY-MM-DD') as "invoice_date", 
+    to_char(t.invoiceDate :: DATE, 'dd-Mon-yyyy') as "invoice_date_en", 
+    t.purchasePrice as "purchase_price", t.netSalesPrice as "invoice_amount", 
+    t.netPurchasePrice as "net_purchase_price", t.payableAmount as "payable_amount", t.receivableAmount as "receivable_amount", 
+    ticket_invoice_received_amount(t.customeroid, t.oid) as "received_amount", 
+    (t.receivableAmount - (ticket_invoice_received_amount(t.customeroid, t.oid))) as "due_amount" 
+    from ${TABLE.TICKET_INVOICE} t 
+    where t.customerOid = $1 and t.companyOid = $2 and t.status = $3 
+    and (t.receivableAmount - (ticket_invoice_received_amount(t.customeroid, t.oid))) > 0`;
 
-    data.push(userInfo.companyoid, CONSTANT.CUSTOMER, CONSTANT.CREDIT_NOTE_ADJUSTMENT, CONSTANT.CASH_ADJUSTMENT);
-
-    let idx = 5;
-
-    if (request.query["referenceOid"]) {
-        query += ` and referenceOid = $${idx++}`
-        data.push(request.query["referenceOid"])
-    }
-    if (request.query["referenceType"]) {
-        query += ` and referenceType = $${idx++}`
-        data.push(request.query["referenceType"])
-    }
-    if (request.query["paymentType"]) {
-        query += ` and paymentType = $${idx++}`
-        data.push(request.query["paymentType"])
-    }
-    if (request.query["paymentNature"]) {
-        query += ` and paymentNature = $${idx++}`
-        data.push(request.query["paymentNature"])
+    if (request.query['search_text']) {
+        const searchText = '%' + request.query['search_text'].trim().toLowerCase() + '%';
+        query += ` and t.invoiceNo ilike $4`;
+        data.push(searchText);
     }
 
-    query += ` order by p.paymentDate desc`;
-
-    if (request.query.offset) {
-        query += ` offset $${idx++}`;
-        data.push(request.query.offset);
-    }
-    if (request.query.limit) {
-        query += ` limit $${idx++}`;
-        data.push(request.query.limit)
-    }
     let sql = {
         text: query,
         values: data
     }
     try {
         list_data = await Dao.get_data(request.pg, sql);
-
     } catch (e) {
         throw new Error(e);
     }
     return list_data;
-}
-module.exports = route_controller;
+};
+
+module.exports = get_list;
